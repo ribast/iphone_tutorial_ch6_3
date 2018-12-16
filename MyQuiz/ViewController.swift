cde00afbcd5d8dd5b8b5f8308a70326274c1083c
田島諒 <ryo_tajima@tajimaryounomac-mini.local>
2018-12-16 14:16:01 +0900

    Initial Commit

---
+++ MyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by 田島諒 on 2018/12/16.
+//  Copyright © 2018 Ribast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
